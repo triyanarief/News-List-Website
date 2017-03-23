3c903c96352ea80e8098f49be93c6b1a0cbbcef7
triyanarief <triyanarief@icloud.com>
2017-03-23 23:22:00 +0700

    adding component

--- vuenews/src/App.vue
+++ vuenews/src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+  <div class="container" id="app">
+    <SourceSelection></SourceSelection>
+    <NewsList></NewsList>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Newslist from './components/Newslist'
+import SourceSelection from './components/SourceSelection'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Newslist,
+    SourceSelection
+  },
+  data () {
+    return {
+      source: ""
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
